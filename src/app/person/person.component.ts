d40c045a7fb0c2a2a0511bd7305eacf355457d09
Wissem Khaled <wissemkhaled69@gmail.com>
2022-02-11 16:23:23 +0100

    Test2

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class PersonComponent implements OnInit {
 
   public name!: String;
-  
+
+  public test!: number;
 
   constructor() { }
 
